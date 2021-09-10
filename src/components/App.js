202015f99eeec1fd9b96ec9592cc49f7f8401edb
Gabriel Neves <gabrielrn.mobres@gmail.com>
2021-09-10 16:16:03 -0300

    add: layout login and signup pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import Navbar from "./Navbar";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useState } from 'react';
+import styled from 'styled-components';
+import LoginPage from "./LoginPage/LoginPage";
+import SignupPage from "./SignupPage/SignupPage";
 
 export default function App() {
 
   return (
     <>
+      <BrowserRouter>
+          <Switch>
+            <Route path="/" exact>
+              <LoginPage />
+            </Route>
+            <Route path="/cadastro" exact>
+              <SignupPage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
     </>
   );
 }
---
+++ src/components/SignupPage/SignupPage.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import logo from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+export default function SignupPage() {
+  
+    return (
+        <Body>
+        <Logo src={logo}/>
+        <Container>
+            <Input type="text"  placeholder="email"/>
+            <Input type="password"  placeholder="senha"/>
+            <Input type="text"  placeholder="nome"/>
+            <Input type="url"  placeholder="foto"/>
+            <Button>Cadastrar</Button>
+            <Link to={`/cadastro`}>
+                <GoTo>Já tem uma conta? Faça login!</GoTo>
+            </Link>
+        </Container>
+    </Body>
+    );
+}
+
+const Body = styled.div`
+    max-width: 375px;
+    margin: auto;
+    font-family: Lexend Deca;
+    font-style: normal;
+    font-weight: normal;
+`
+
+const Container = styled.div`
+    flex-direction: column;
+    justify-content: center;
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const Logo = styled.img`
+    margin-top: 68px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 180px;
+    height: auto;
+    margin-bottom: 28px;
+`
+const Input = styled.input`
+    width: 100%;
+    height: 45px;
+    background: #FFFFFF;
+    border: 1px solid #D5D5D5;
+    box-sizing: border-box;
+    border-radius: 5px;
+    margin: 3px;
+    font-size: 20px;
+    padding-left: 10px;
+
+    ::placeholder {
+    color: #DBDBDB;
+    }
+`
+
+const Button = styled.button`
+    width: 100%;
+    height: 45px;
+    margin: 3px;
+    background: #52B6FF;
+    cursor: pointer;
+    border: none;
+    border-radius: 5px;
+    font-size: 20px;
+    text-align: center;
+    color: #FFFFFF;
+`
+
+const GoTo = styled.p`
+    margin-top: 22px;
+    font-size: 14px;
+    text-align: center;
+    text-decoration-line: underline;
+    color: #52B6FF;
+    cursor: pointer;
+`
